371c0a675e588e7d935e15ac3a74debc95f27620
harsh9200 <hagarwal9200@gmail.com>
2024-09-02 05:18:58 +0530

    Add d/w/s instructions to schema

--- orca-whirlpool/src/db.rs
+++ orca-whirlpool/src/db.rs
@@ -1,12 +1,11 @@
 use crate::{
-    constants,
     key_store::StoreKey,
-    pb::messari::orca_whirlpool::v1::{Pool, Pools},
+    pb::messari::orca_whirlpool::v1::{Deposits, Pool, Pools, Swaps, Withdraws},
 };
 
 use substreams::{
     key,
-    scalar::BigInt,
+    scalar::{BigDecimal, BigInt},
     store::{DeltaBigInt, DeltaExt, DeltaInt64, Deltas, StoreGetProto},
 };
 use substreams_entity_change::tables::Tables;
@@ -15,14 +14,13 @@ pub fn handle_protocol_entity(
     tables: &mut Tables,
     users_delta: Deltas<DeltaInt64>,
     pools_delta: Deltas<DeltaInt64>,
+    protocol_id: &String,
     is_initialized: bool,
 ) {
-    let id = bs58::encode(constants::ORCA_WHIRLPOOL).into_string();
-
     // Create protocol entity if not initialized
     if !is_initialized {
         tables
-            .create_row("Protocol", &id)
+            .create_row("Protocol", protocol_id)
             .set("cumulativeUniqueUsers", BigInt::zero())
             .set("totalPoolCount", BigInt::zero());
 
@@ -32,14 +30,14 @@ pub fn handle_protocol_entity(
     // Update cumulative unique users
     users_delta.iter().for_each(|delta| {
         tables
-            .update_row("Protocol", &id)
+            .update_row("Protocol", protocol_id)
             .set("cumulativeUniqueUsers", delta.new_value);
     });
 
     // Update total pool count
     pools_delta.iter().for_each(|delta| {
         tables
-            .update_row("Protocol", &id)
+            .update_row("Protocol", protocol_id)
             .set("totalPoolCount", delta.new_value);
     });
 }
@@ -50,13 +48,12 @@ pub fn handle_pool_entity(
     pools_store: StoreGetProto<Pool>,
     pool_balances_delta: Deltas<DeltaBigInt>,
     pool_liquidity_delta: Deltas<DeltaBigInt>,
+    protocol_id: &String,
 ) {
-    let protocol_id = bs58::encode(constants::ORCA_WHIRLPOOL).into_string();
-
     initialized_pools.data.iter().for_each(|pool| {
         tables
             .create_row("Pool", &pool.address)
-            .set("protocol", &protocol_id)
+            .set("protocol", protocol_id)
             .set(
                 "inputTokens",
                 vec![pool.token_mint_a.clone(), pool.token_mint_b.clone()],
@@ -105,4 +102,89 @@ pub fn handle_pool_entity(
                 .update_row("Pool", pool)
                 .set("outputTokenSupply", &delta.new_value);
         });
+}
+
+pub fn handle_deposit_entity(tables: &mut Tables, map_deposits: Deposits, protocol_id: &String) {
+    map_deposits.data.iter().for_each(|deposit| {
+        tables
+            .create_row("Deposit", &deposit.id)
+            .set("blockHash", &deposit.block_hash)
+            .set("protocol", protocol_id)
+            .set("to", &deposit.to)
+            .set("from", &deposit.from)
+            .set("slot", BigInt::from(deposit.slot))
+            .set("blockNumber", BigInt::from(deposit.block_height))
+            .set("timestamp", BigInt::from(deposit.block_timestamp))
+            .set(
+                "inputTokens",
+                vec![deposit.token_a.clone(), deposit.token_b.clone()],
+            )
+            .set("outputToken", &deposit.to)
+            .set(
+                "inputTokenAmounts",
+                vec![
+                    BigInt::try_from(deposit.amount_a.clone()).unwrap(),
+                    BigInt::try_from(deposit.amount_b.clone()).unwrap(),
+                ],
+            )
+            .set(
+                "outputTokenAmount",
+                BigInt::try_from(deposit.output_amount.clone()).unwrap(),
+            )
+            .set("amountUSD", &BigDecimal::zero())
+            .set("pool", &deposit.to);
+    });
+}
+
+pub fn handle_withdraw_entity(tables: &mut Tables, map_withdraws: Withdraws, protocol_id: &String) {
+    map_withdraws.data.iter().for_each(|withdraw| {
+        tables
+            .create_row("Withdraw", &withdraw.id)
+            .set("blockHash", &withdraw.block_hash)
+            .set("protocol", protocol_id)
+            .set("to", &withdraw.to)
+            .set("from", &withdraw.from)
+            .set("slot", BigInt::from(withdraw.slot))
+            .set("blockNumber", BigInt::from(withdraw.block_height))
+            .set("timestamp", BigInt::from(withdraw.block_timestamp))
+            .set(
+                "inputTokens",
+                vec![withdraw.token_a.clone(), withdraw.token_b.clone()],
+            )
+            .set("outputToken", &withdraw.to)
+            .set(
+                "inputTokenAmounts",
+                vec![
+                    BigInt::try_from(withdraw.amount_a.clone()).unwrap(),
+                    BigInt::try_from(withdraw.amount_b.clone()).unwrap(),
+                ],
+            )
+            .set(
+                "outputTokenAmount",
+                BigInt::try_from(withdraw.output_amount.clone()).unwrap(),
+            )
+            .set("amountUSD", &BigDecimal::zero())
+            .set("pool", &withdraw.to);
+    });
+}
+
+pub fn handle_swap_entity(tables: &mut Tables, map_swaps: Swaps, protocol_id: &String) {
+    map_swaps.data.iter().for_each(|swap| {
+        tables
+            .create_row("Swap", &swap.id)
+            .set("blockHash", &swap.block_hash)
+            .set("protocol", protocol_id)
+            .set("to", &swap.to)
+            .set("from", &swap.from)
+            .set("slot", BigInt::from(swap.slot))
+            .set("blockNumber", BigInt::from(swap.block_height))
+            .set("timestamp", BigInt::from(swap.block_timestamp))
+            .set("tokenIn", &swap.token_in)
+            .set_bigint("amountIn", &swap.amount_in)
+            .set("amountInUSD", &BigDecimal::zero())
+            .set("tokenOut", &swap.token_out)
+            .set_bigint("amountOut", &swap.amount_out)
+            .set("amountOutUSD", &BigDecimal::zero())
+            .set("pool", &swap.to);
+    });
 }
--- orca-whirlpool/src/modules/100_graph_out.rs
+++ orca-whirlpool/src/modules/100_graph_out.rs
@@ -1,5 +1,5 @@
-use crate::db;
-use crate::pb::messari::orca_whirlpool::v1::{Pool, Pools};
+use crate::pb::messari::orca_whirlpool::v1::{Deposits, Pool, Pools, Swaps, Withdraws};
+use crate::{constants, db};
 
 use substreams::pb::substreams::Clock;
 use substreams::skip_empty_output;
@@ -16,16 +16,21 @@ fn graph_out(
     total_pool_count_delta: Deltas<DeltaInt64>,
     pool_balances_delta: Deltas<DeltaBigInt>,
     pool_liquidity_delta: Deltas<DeltaBigInt>,
+    map_deposits: Deposits,
+    map_withdraws: Withdraws,
+    map_swaps: Swaps,
 ) -> Result<EntityChanges, ()> {
     skip_empty_output();
 
     let mut tables = Tables::new();
     let is_initialized = clock.number != 124152351;
+    let protocol_id = bs58::encode(constants::ORCA_WHIRLPOOL).into_string();
 
     db::handle_protocol_entity(
         &mut tables,
         cumulative_users_delta,
         total_pool_count_delta,
+        &protocol_id,
         is_initialized,
     );
     db::handle_pool_entity(
@@ -34,7 +39,12 @@ fn graph_out(
         pools_store,
         pool_balances_delta,
         pool_liquidity_delta,
+        &protocol_id,
     );
+
+    db::handle_deposit_entity(&mut tables, map_deposits, &protocol_id);
+    db::handle_withdraw_entity(&mut tables, map_withdraws, &protocol_id);
+    db::handle_swap_entity(&mut tables, map_swaps, &protocol_id);
 
     Ok(tables.to_entity_changes())
 }
