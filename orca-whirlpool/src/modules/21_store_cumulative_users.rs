fedb49aa07a4418a4a7cc4286e4f948a64297b4d
harsh9200 <hagarwal9200@gmail.com>
2024-09-03 05:19:30 +0530

    Phase 3: Begins

--- orca-whirlpool/src/modules/21_store_cumulative_users.rs
+++ orca-whirlpool/src/modules/21_store_cumulative_users.rs
@@ -1,14 +1,35 @@
+use substreams::pb::substreams::Clock;
 use substreams::skip_empty_output;
-use substreams::store::{DeltaBigInt, Deltas, StoreAddInt64};
+use substreams::store::{DeltaBigInt, Deltas, StoreAddInt64, StoreDelete};
 use substreams::store::{StoreAdd, StoreNew};
 
 use crate::key_store::StoreKey;
 
 #[substreams::handlers::store]
-pub fn store_cumulative_users(unique_users_delta: Deltas<DeltaBigInt>, store: StoreAddInt64) {
+pub fn store_cumulative_users(
+    clock: Clock,
+    unique_users_delta: Deltas<DeltaBigInt>,
+    store: StoreAddInt64,
+) {
     skip_empty_output();
 
+    let day_id = clock.timestamp.unwrap().seconds / 86400;
+
+    // Delete previous day's snapshot
+    store.delete_prefix(
+        0,
+        &StoreKey::UsageMetricsDailySnapshot(day_id - 1, None).unique_id(),
+    );
+
     for _ in unique_users_delta.deltas {
-        store.add(0, StoreKey::CumulativeUsers.unique_id(), 1)
+        store.add_many(
+            0,
+            &vec![
+                StoreKey::CumulativeUsers.unique_id(),
+                StoreKey::UsageMetricsDailySnapshot(day_id, Some(Box::new(StoreKey::ActiveUsers)))
+                    .get_snapshot_key(None),
+            ],
+            1,
+        );
     }
 }
